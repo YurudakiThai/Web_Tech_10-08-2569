24947fa6b97e701470f51f155b206d1c54effddf
Sapon <sapon@MacBookAirSapon.local>
2026-08-11 10:47:51 +0700

    change_index.html_algo_form.js

--- file_javascript/algo_form.js
+++ file_javascript/algo_form.js
@@ -1,11 +1,10 @@
-const form = document.querySelector("#main_from");
+const form = document.querySelector("#transactionForm");
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const name = document.querySelector('#name').value;
   const amount =  document.querySelector('#amout').value;
   const date = document.querySelector('#dates').value;
-
   const times = document.querySelector('#times').value;
 
 console.log(name);
